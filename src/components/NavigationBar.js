45262604141c457428216cb01b693cff878981f8
I Kadek Candra Gunawan <kadekcandra1630@gmail.com>
2026-02-14 06:20:40 -0600

    add new commit

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,20 +1,44 @@
-import { Navbar, Container, Nav } from "react-bootstrap"
+import {
+  Navbar,
+  Container,
+  Nav,
+  Row,
+  Col,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 const NavigationBar = () => {
   return (
     <div>
-      <Navbar variant="dark">
+      <Navbar variant="dark" className="justify-content-between">
         <Container>
-          
           <Navbar.Brand href="/">PAKELFILMS</Navbar.Brand>
           <Nav>
             <Nav.Link href="#trending">TRENDING</Nav.Link>
             <Nav.Link href="#superhero">SUPERHERO</Nav.Link>
           </Nav>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Control
+                  type="input"
+                  placeholder="search.."
+                  className="bg-transparent"
+                />
+              </Col>
+              <Col>
+                <Button variant="light" onClick={() => console.log("search")}>
+                  search
+                </Button>
+              </Col>
+            </Row>
+          </Form>
         </Container>
       </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
